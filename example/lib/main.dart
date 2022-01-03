bf34bc480fed57f9b499cfe73d15fff09f4843fe
Kushal Mahapatro <kushalmahapatro@gmail.com>
2022-01-03 09:47:25 +0400

    new version of flutter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,161 +1,115 @@
-import 'package:cuberto_bottom_bar/cuberto_bottom_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarColor: Colors.black.withOpacity(1),
-        statusBarIconBrightness: Brightness.light));
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.light(),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title: ''}) : super(key: key);
-  final String? title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  int currentPage = 0;
-  Color currentColor = Colors.deepPurple;
-  Color inactiveColor = Colors.black;
-  late PageController tabBarController;
-  List<Tabs> tabs = <Tabs>[];
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    currentPage = 0;
-    tabs.add(Tabs(
-      Icons.home,
-      "Home",
-      Colors.deepPurple,
-      getGradient(Colors.deepPurple),
-    ));
-    tabs.add(
-        Tabs(Icons.search, "Search", Colors.pink, getGradient(Colors.pink)));
-    tabs.add(
-        Tabs(Icons.alarm, "Alarm", Colors.amber, getGradient(Colors.amber)));
-    tabs.add(Tabs(
-        Icons.settings, "Settings", Colors.teal, getGradient(Colors.teal)));
-    tabBarController = new PageController(initialPage: 0);
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget tabView({int destinationIndex: 0}) {
-      return Container(
-          decoration: BoxDecoration(color: tabs[currentPage].color),
-          child: InkWell(
-              child: Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    tabs[currentPage].title,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w700),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    "Click here to Change the tab To " +
-                        tabs[destinationIndex].title,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white, fontSize: 14.0),
-                  ),
-                ],
-              )),
-              onTap: () {
-                setState(() {
-                  currentPage = destinationIndex;
-                  tabBarController.jumpToPage(currentPage);
-                });
-              }));
-    }
-
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      body: PageView(
-          controller: tabBarController,
-          physics: NeverScrollableScrollPhysics(),
-          children: <Widget>[
-            tabView(destinationIndex: 3),
-            tabView(destinationIndex: 0),
-            tabView(destinationIndex: 1),
-            tabView(destinationIndex: 2)
-          ]),
-      drawer: new Container(
-          width: 250.0,
-          margin: EdgeInsets.only(bottom: 60.0),
-          color: Colors.blue,
-          child: ListView(
-            children: <Widget>[Text("Hello"), Text("World")],
-          )),
-      endDrawer: new Container(
-          width: 250.0,
-          margin: EdgeInsets.only(bottom: 60.0),
-          color: Colors.blue,
-          child: ListView(
-            children: <Widget>[Text("Hello"), Text("World")],
-          )),
-      bottomNavigationBar: CubertoBottomBar(
-        key: Key("BottomBar"),
-        inactiveIconColor: inactiveColor,
-        tabStyle: CubertoTabStyle.STYLE_FADED_BACKGROUND,
-        selectedTab: currentPage,
-        tabs: tabs
-            .map((value) => TabData(
-                key: Key(value.title),
-                iconData: value.icon,
-                title: value.title,
-                tabColor: value.color,
-                tabGradient: value.gradient))
-            .toList(),
-        onTabChangedListener: (position, title, color) {
-          setState(() {
-            currentPage = position;
-            tabBarController.jumpToPage(position);
-          });
-        },
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
-  @override
-  void dispose() {
-    tabBarController.dispose();
-    super.dispose();
-  }
-}
-
-class Tabs {
-  final IconData icon;
-  final String title;
-  final Color color;
-  final Gradient gradient;
-
-  Tabs(this.icon, this.title, this.color, this.gradient);
-}
-
-getGradient(Color color) {
-  return LinearGradient(
-      colors: [color.withOpacity(0.5), color.withOpacity(0.1)],
-      stops: [0.0, 0.7]);
 }
